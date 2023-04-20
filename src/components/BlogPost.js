4b26adfb59ab70eb5c4cdca11750d06ad015367b
martinmwas254 <martinmwas264@gmail.com>
2023-04-20 21:48:07 +0300

    complete assignment

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -5,10 +5,22 @@ import Comment from "./Comment";
 function BlogPost() {
   return (
     <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
-      <Comment commentText="I agree with this statement. - Angela Merkel" />
-      <Comment commentText="A universal truth. - Noam Chomsky" />
-      <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      <BlogContent
+        articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration."
+        isPublished={true}
+        minutesToRead={1}
+      />
+      <Comment text="I agree with this statement. - Angela Merkel" />
+      <Comment text="A universal truth. - Noam Chomsky" />
+      <Comment text="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+    </div>
+  );
+}
+
+export function ChildComponent(props) {
+  return (
+    <div>
+      {props.text} {props.number}
     </div>
   );
 }
--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Comment(props) {
-  return <div className="comment">{props.commentText}</div>;
+  return <div>{props.text}</div>;
 }
 
-export default Comment;
+function BlogPost() {
+  return (
+    <div>
+      <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+      <Comment text="I agree with this statement. - Angela Merkel" />
+      <Comment text="A universal truth. - Noam Chomsky" />
+      <Comment text="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+    </div>
+  );
+}
+
+export default BlogPost;
